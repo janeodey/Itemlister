5b13e31987e3a77d2b61fd8f82dedaca60a4a137
Jane Odey <janeodeys98@gmail.com>
2025-12-14 08:05:03 +0100

    script:added more functionalities

--- list-item.js
+++ list-item.js
@@ -1,106 +1,10 @@
 
-// // // console.dir(document)
-
-// // // console.log(document.doctype);
-// // // getElementsByTagName("li") works thesame way as getElementsBy ClassName("list-group-item")
-
-// // let headerBorder = document.getElementById("header");
-// // headerBorder.style.borderBottom = "3px solid #000"
-
-// // let lists = document.getElementsByClassName("list-group-item")
-
-// // lists[1].textContent="Jane"
-
-// // for(let i = 0; i < lists.length;i++){
-// //     lists[i].style.backgroundColor="#f4f4f4"
-// // }
-
-// // let sends = document.querySelector("input[type='submit'")
-// // sends.value="SEND"
-
-// // // let lastItem =document.querySelector(".list-group-item:last-child");
-
-// // // lastItem.style.backgroundColor="purple"
-
-// // let odd = document.querySelectorAll("li.list-group-item:nth-child(odd)");
-
-// // for(let i =0; i<odd.length; i++){
-// //     odd[i].style.backgroundColor="#ccc"
-// // }
-
-
-// let parents = document.querySelector(".list-items")
-
-// // console.log(parents.parentNode.parentNode);
-// // console.log(parents.parentElement.parentElement);
-
-// parents.parentElement.style.backgroundColor="#ccc"
-// parents.style.backgroundColor="#c1c1c1"
-
-// // creata a div
-// let newDiv = document.createElement("div")
-// let newImg = document.createElement("img")
-
-// newDiv.className = "Hello";
-// newDiv.id="greetings";
-
-// // add attribute
-// newDiv.setAttribute("title","myname");
-// // newImg.setAttribute("src","v.png");
-
-// // create a text note
-// let textNode = document.createTextNode("Hello World!");
-
-// // add text to div
-// newDiv.appendChild(textNode)
-
-// let container = document.querySelector(".containers h2");
-// let h1 = document.querySelector("#containers h3")
-
-// container.insertBefore(newDiv, h1)
-
-// // container.appendChild(newDiv);
-
-
-// console.log(newDiv);
-
-// let btn = document.getElementById("btn")
-// let box = document.getElementById("box")
-// let itemsInput = document.getElementById("itemsInput");
-
-// box.addEventListener("mouseenter", myFunction)
-// box.addEventListener("mouseleave", myFunction)
-// box.addEventListener("mousemove", myFunction)
-// itemsInput.addEventListener("keydown", myFunction)
-
-// function myFunction(e){
-//     console.log(e)
-//     console.log(e.target.value);
-    
-    // console.log(e.target.id);
-    // console.log(e.type);
-    // let output = document.getElementById("output")
-    // output.innerHTML= ` <h1>${e.target.id}</h1>`
-    // let output= document.getElementById("output")
-    // output.innerHtml = `<h1>MouseX: ${e.offsetX}</h1> <h1>MouseY: ${e.offsetY}</h1>`
-    // output.innerHTML = '<h3>MouseX: '+e.offsetX+'</h3><h3>Mousey:'+e.offsetY+'</h3>'
-    // let output = document.getElementById("output")
-    // output.innerHTML = e.type
-    // output.innerHTML = "<h3>MouseX:" +e.offsetX+ "</h3><h3>MouseY:" +e.offsetY+"</h3>"
-    // output.innerHTML = `<h2>MouseX: ${e.offsetX}</h2> <h2>MouseY: ${e.offsetY}</h2>`
-    // box.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, ${40})`;
-    // box.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
-    
-// }
-
-
-// let header = document.getElementById("header-title").add
-
-
-
 let form = document.getElementById("addForm")
 let listItems = document.getElementById("itemsUl")
 let filter = document.getElementById("filterItem")
+
+// local storage
+document.addEventListener("DOMContentLoaded", loadItems);
 
 let editMode = false;
 let itemToEdit = null
@@ -117,43 +21,103 @@ filter.addEventListener("keyup", filterItems)
 // edit items
 // listItems.addEventListener("click", updateItem)
 
+// load items from local storage
+function loadItems(){
+    let items = JSON.parse(localStorage.getItem("items")) || [];
+
+    items.forEach(item=>{
+        addItemToDOM(item)
+    })
+}
+
+// create a reusable function to add items to DOM
+function addItemToDOM(text){
+    let li = document.createElement("li")
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(text));
+
+    let btnContainer = document.createElement("div")
+
+    let editBtn = document.createElement("button")
+    editBtn.className = "btnEdit";
+    editBtn.textContent = "Edit";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.className = "btnlist"
+    deleteBtn.textContent = "X"
+
+    btnContainer.appendChild(editBtn)
+    btnContainer.appendChild(deleteBtn)
+
+    li.appendChild(btnContainer)
+    listItems.appendChild(li)
+}
+
+// save items to localStorage
+function getStoredItems(){
+    return JSON.parse(localStorage.getItem("items")) || []
+}
+
+function saveItems(items){
+    localStorage.setItem("items", JSON.stringify(items))
+}
+
+
 function todoList(e){
     e.preventDefault()
 
     // get input value
     let listValues = document.getElementById("itemsInput").value.trim();
-    if(listValues === "") return
+    if(listValues === "") return;
+
+    let items = getStoredItems();
 
     if(editMode){
+        let oldText = itemToEdit.firstChild.textContent.trim()
+
+        // update DOM
         itemToEdit.firstChild.textContent=listValues;
+
+        // Update localstorage
+        let index = items.indexOf(oldText)
+        if(index > -1){
+            items[index] = listValues;
+        }
 
         editMode =false;
         itemToEdit = null
         document.querySelector("input[type='submit']").value = "Submit"
     }
     else{
+
+        addItemToDOM(listValues);
+        items.push(listValues);
+        
         // create new li element
-    let li = document.createElement("li")
-    li.className = "list-group-item";    
+    // let li = document.createElement("li")
+    // li.className = "list-group-item";    
 
-    li.appendChild(document.createTextNode(listValues))
+    // li.appendChild(document.createTextNode(listValues))
 
-    // create edit and delete buttons
-    let deleteBtn = document.createElement("button");
-    let editBtn = document.createElement("button")
+    // // create edit and delete buttons
+    // let deleteBtn = document.createElement("button");
+    // let editBtn = document.createElement("button")
 
-    deleteBtn.className = "btnlist"
-    editBtn.className = "btnEdit"
+    // deleteBtn.className = "btnlist"
+    // editBtn.className = "btnEdit"
 
-    editBtn.appendChild(document.createTextNode("Edit"))
-    deleteBtn.appendChild(document.createTextNode("X"))
+    // editBtn.appendChild(document.createTextNode("Edit"))
+    // deleteBtn.appendChild(document.createTextNode("X"))
    
-    li.appendChild(editBtn)
-    li.appendChild(deleteBtn)
-    // list main container
-    listItems.appendChild(li)
+    // li.appendChild(editBtn)
+    // li.appendChild(deleteBtn)
+    // // list main container
+    // listItems.appendChild(li)
     }
+    saveItems(items);
 
+    // input.value = "";
+    // input.focus();
     document.getElementById("itemsInput").value = "";
     document.getElementById("itemsInput").focus()
 }
@@ -163,13 +127,22 @@ function todoList(e){
 
 
 function manageItem(e){
+    let items = getStoredItems();
+
+    // delete
     if(e.target.classList.contains("btnlist")){
         // if(confirm("are you sure?")){
         //     let li = e.target.parentElement;
         //     listItems.removeChild(li)
         // }
         // e.target.parentElement.parentElement.remove()
-        e.target.closest("li").remove();
+        let li = e.target.closest("li");
+        let text = li.firstChild.textContent.trim()
+
+        li.remove();
+
+        items = items.filter(item => item !== text);
+        saveItems(items)
     }
 
     // edit item
@@ -178,6 +151,7 @@ function manageItem(e){
 
         // itemToEdit = e.target.parentElement.parentElement;
         itemToEdit = e.target.closest("li")
+
         let oldText = itemToEdit.firstChild.textContent.trim();
 
         document.getElementById("itemsInput").value = oldText;
